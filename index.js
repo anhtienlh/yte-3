f8b3da942789a0f7693b34827dbc8d9124533a5d
lhtien <tienlhy@techvn.mti.co.jp>
2023-02-18 22:13:44 +0700

    init source code

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const compression = require('compression')
+
+// setup serve static file
+const app = express();
+app.use(compression());
+const port = process.env.PORT || 80;
+
+app.use(express.static(path.join(__dirname + '/public')))
+
+// app.get('/', (req, res) => res.send(prepareHtml));
+
+app.get('/', function (req, res, next) {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.listen(port, () => console.log(`YTE-3 export listening at http://localhost:${port}`))
