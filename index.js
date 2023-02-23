4d3bafd5ba51d2d8fa93e9abe2b8ee10c0e18a49
lhtien <tienlhy@techvn.mti.co.jp>
2023-02-23 14:01:08 +0700

    update UI for requirement 2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const compression = require('compression')
 // setup serve static file
 const app = express();
 app.use(compression());
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname + '/public')))
 
